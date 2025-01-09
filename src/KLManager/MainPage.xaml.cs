7fc30a186ade895d8912315c3eb9b618e4cd02f5
zyf <3014085426@qq.com>
2025-01-09 12:59:47 +0800

    commit

---
+++ src/KLManager/MainPage.xaml.cs
@@ -0,0 +1,25 @@
+﻿using ZXing;
+using ZXing.Net.Maui;
+
+namespace KLManager;
+
+public partial class MainPage : ContentPage
+{
+    int count = 0;
+
+    public MainPage()
+    {
+        InitializeComponent();
+        camera.Options = new BarcodeReaderOptions
+        {
+            Formats = BarcodeFormats.TwoDimensional,
+            AutoRotate = true,
+            Multiple = false,
+        };
+    }
+
+    private void camera_BarcodesDetected(
+        object sender,
+        ZXing.Net.Maui.BarcodeDetectionEventArgs e
+    ) { }
+}
