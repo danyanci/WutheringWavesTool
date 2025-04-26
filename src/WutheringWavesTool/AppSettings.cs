fc6abbb7cea450415ca629ca7341d9f1a7ff21ae
俎羽峰 <3014085426@qq.com>
2025-04-26 10:15:22 +0800

    commit

--- src/WutheringWavesTool/AppSettings.cs
+++ src/WutheringWavesTool/AppSettings.cs
@@ -1,32 +1,43 @@
-﻿namespace WutheringWavesTool;
+﻿using SqlSugar;
+
+namespace WutheringWavesTool;
 
 public class AppSettings
 {
     public static ApplicationDataContainer LocalSetting =>
         Windows.Storage.ApplicationData.Current.LocalSettings;
 
+    public static ISqlSugarClient SettingDB =>
+        new SqlSugarScope(
+            new ConnectionConfig()
+            {
+                ConnectionString = $"DataSource={App.BassFolder}\\System.db",
+                DbType = DbType.Sqlite,
+            }
+        );
+
     public static string? Token
     {
-        get => Read();
-        set => Write(value);
+        get => ReadDb();
+        set => WriteDb(value);
     }
 
     public static string? TokenId
     {
-        get => Read();
-        set => Write(value);
+        get => ReadDb();
+        set => WriteDb(value);
     }
 
     public static string? WallpaperPath
     {
-        get => Read();
-        set => Write(value);
+        get => ReadDb();
+        set => WriteDb(value);
     }
 
     public static string? AppTheme
     {
-        get => Read();
-        set => Write(value);
+        get => ReadDb();
+        set => WriteDb(value);
     }
 
     internal static string? Read([CallerMemberName] string Path = null)
@@ -36,6 +47,34 @@ public class AppSettings
             return value.ToString();
         }
         return null;
+    }
+
+    internal static string? ReadDb([CallerMemberName] string key = null)
+    {
+        SettingDB.CodeFirst.InitTables<LocalSettings>();
+        var result = SettingDB.Queryable<LocalSettings>().Where(x => x.Key == key);
+        if (result.Count() == 1)
+        {
+            return result.First().Value;
+        }
+        return null;
+    }
+
+    internal static void WriteDb(string? value, [CallerMemberName] string key = null)
+    {
+        SettingDB.CodeFirst.InitTables<LocalSettings>();
+        var result = SettingDB.Queryable<LocalSettings>().Where(x => x.Key == key).First();
+
+        if (result != null)
+        {
+            result.Value = value;
+            SettingDB.Updateable(result).ExecuteCommand();
+        }
+        else
+        {
+            var newSetting = new LocalSettings { Key = key, Value = value };
+            SettingDB.Insertable(newSetting).ExecuteCommand();
+        }
     }
 
     internal static void Write(string? value, [CallerMemberName] string path = null)
